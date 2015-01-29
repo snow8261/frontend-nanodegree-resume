8086ad66c532d1d369a9b6c8dc3512f5a61a9873
TongjieLi <snow8261@gmail.com>
2015-01-29 16:28:21 +0800

    json

---
+++ js/resumeBuilder.js
@@ -0,0 +1,44 @@
+var bio={
+	"name":"litongjie",
+	"role":"developer",
+	"contact":{
+	"mail":"sss@gmail.com",
+	"address": "shanghai"
+	},
+	"image":"images/fry.jpg",
+	"welcome message":"hello there",
+	"skills":["programming","testing"]
+}
+
+var work={
+	"jobs":[{
+    "job position":"project manager",
+	"employer":"CCSSOFT",
+	"years":"7",
+	"city":"Shanghai"
+	}]
+}
+
+var education={
+	"schools":[
+	{
+	"name":"kiel",
+	"years":7,
+	"city":"kiel",
+	"degree":"MS"
+ 	},
+ 	{
+ 	"name":"tongji university",
+ 	"city":"shanghai",
+ 	"years":4,
+ 	"degree":"BS"
+ 	}
+ 	]
+}
+
+// var formattedName=HTMLheaderName.replace("%data%",bio.name);
+// var formattedRole=HTMLheaderRole.replace("%data%",bio.role);
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+// $("#main").append(work["job position"]);
+// $("#main").append(education.name);
